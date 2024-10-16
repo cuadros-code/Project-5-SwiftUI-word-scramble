439e61a94ac683709448ecf76d5e74590ca70c8f
kevin cuadros <kevindavidcuadros@gmail.com>
2024-10-15 19:21:43 -0500

    Initial Commit

---
+++ Project-5-SwiftUI-word-scramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-5-SwiftUI-word-scramble
+//
+//  Created by Kevin Cuadros on 15/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-5-SwiftUI-word-scramble/Project_5_SwiftUI_word_scrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_5_SwiftUI_word_scrambleApp.swift
+//  Project-5-SwiftUI-word-scramble
+//
+//  Created by Kevin Cuadros on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Project_5_SwiftUI_word_scrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
